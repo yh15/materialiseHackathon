e282cd7a533c3fa8e998af34842d77283d0b23fd
Toong Chee Sheng <zhicheng.88@hotmail.com>
2016-02-20 23:34:03 +0800

    added searchtest.php for user-input search

--- application/controllers/dbcontroltest.php
+++ application/controllers/dbcontroltest.php
@@ -4,8 +4,8 @@ class dbcontroltest extends CI_Controller {
         public function index()
         {
 				$this->load->model('dbquery');
-				$searchterm = 'orange'; //term to search
-				$sel = 'name'; //name:name, cat:category
+				$searchterm = $_POST["term"]; //term to search
+				$sel = $_POST["type"]; //name:name, cat:category
 				
 				if($sel=='name'){
 					$data['results'] = $this->dbquery->searchByName($searchterm);
