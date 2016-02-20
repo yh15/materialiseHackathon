9b5c176d8422d017f71b9ca6dc6ac2189649f41d
YH15 <yinghui.lew@gmail.com>
2016-02-20 17:35:54 +0800

    update config file for base_url. Change index.html to index.php

--- application/controllers/DatabaseTest.php
+++ application/controllers/DatabaseTest.php
@@ -1,18 +1,17 @@
 <?php
 class DatabaseTest extends CI_Controller {
-
-        public function index()
-        {
-                $this->load->database();
-				$query = $this->db->query('SELECT ob_name, id, category FROM datatable');
-
-				foreach ($query->result() as $row)
-				{	//no new line
-					echo $row->ob_name;
-					echo $row->id;
-					echo $row->category;
-				}
-
-				echo 'Total Results: ' . $query->num_rows();
-        }
+	
+	public function index() {
+		$this->load->database ();
+		$query = $this->db->query ( 'SELECT ob_name, id, category FROM datatable' );
+		
+		foreach ( $query->result () as $row ) { // no new line
+			echo $row->ob_name;
+			echo $row->id;
+			echo $row->category;
+		}
+		
+		echo 'Total Results: ' . $query->num_rows ();
+	}
 }
+?>
